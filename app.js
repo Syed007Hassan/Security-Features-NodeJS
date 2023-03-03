c14af11087bced2d7416182aff07a2c9bf2fc67a
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-03-04 02:07:48 +0500

    tls certificates added

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ express.static("public");
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const app = require("./app");
-
 const https = require("https");
+const fs = require("fs");
 
 const PORT = process.env.PORT || 5000;
 
-https.createServer({}, app).listen(PORT, () => {
-  console.log("Listening on port " + PORT + "...");
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.cert"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log("Listening on port " + PORT + "...");
+  });
